ba215933176e1e8c75690f09fe9b132c7e3b0237
hans <hans.bourgeois@ulb.be>
2022-02-17 20:37:25 +0100

    17-02-22 add ft_putnbr_unsigned_fd to libft

--- ft_printf_p.c
+++ ft_printf_p.c
@@ -6,7 +6,7 @@
 /*   By: hbourgeo <hbourgeo@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 09:05:48 by hbourgeo          #+#    #+#             */
-/*   Updated: 2022/02/17 19:42:13 by hbourgeo         ###   ########.fr       */
+/*   Updated: 2022/02/17 20:32:50 by hbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@ int ft_printf_p (va_list arg)
 
 	size = 0;
 	add = va_arg(arg, unsigned long long int);
+	if (add == 0)
+	{
+		size += ft_putstr_fd("0x0", 1);
+		return(size);
+	}
 	size += ft_putstr_fd("0x", 2);
 	size += ft_putnbr_base_fd (add, "0123456789abcdef", 1);
 	return(size);
--- ft_printf_s.c
+++ ft_printf_s.c
@@ -6,7 +6,7 @@
 /*   By: hbourgeo <hbourgeo@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 09:05:56 by hbourgeo          #+#    #+#             */
-/*   Updated: 2022/02/17 20:01:36 by hbourgeo         ###   ########.fr       */
+/*   Updated: 2022/02/17 20:33:36 by hbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ int ft_printf_s (va_list arg)
 
 	size = 0;
 	str = va_arg(arg, char *);
-	if (str == 0)
-		size += ft_putstr_fd("(null)", 1);
+	if (str == NULL)
+	{
+		size += ft_putstr_fd("(null)",1);
+		return (size);
+	}
 	while (*str)
 	{
 		size += write(1,str,1);
