fc0c3bec463396d0a21b107ce8ee32c81bc70f18
hans <hans.bourgeois@ulb.be>
2022-02-17 19:55:18 +0100

    17-02-22 add ft_putnbr_unsigned_fd to libft

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: hbourgeo <hbourgeo@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/15 08:42:18 by hbourgeo          #+#    #+#             */
-/*   Updated: 2022/02/17 19:52:56 by hbourgeo         ###   ########.fr       */
+/*   Updated: 2022/02/17 19:55:14 by hbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
 #include <stdarg.h>
 #include "libft/libft.h"
+#include <stdlib.h>
 
 int	ft_printf_c(va_list arg);
 int ft_printf_d (va_list arg);
