a455be63ee1ede0c7fc3266ed9fdc0ce265bfd0f
hans <hans.bourgeois@ulb.be>
2022-03-20 15:51:15 +0100

    20-03-22 | remove '0x' from ft_print_x_up

--- ft_printf_x_up.c
+++ ft_printf_x_up.c
@@ -6,7 +6,7 @@
 /*   By: hbourgeo <hbourgeo@student.19.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/17 09:03:07 by hbourgeo          #+#    #+#             */
-/*   Updated: 2022/02/18 10:23:59 by hbourgeo         ###   ########.fr       */
+/*   Updated: 2022/03/20 15:48:39 by hbourgeo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ int	ft_printf_x_up(va_list arg)
 
 	size = 0;
 	hex_up = (unsigned int)va_arg(arg, int);
-	size = ft_putstr_fd("0x", 2);
 	size = ft_putnbr_base_fd(hex_up, "0123456789ABCDEF", 1);
 	return (size);
 }
